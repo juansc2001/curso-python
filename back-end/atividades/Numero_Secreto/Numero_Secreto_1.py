b9df699ad71b2c56f1f12c88e696fdd2473cdcbf
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-01-10 15:34:59 -0300

    exercicios_funcoes adicionado

---
+++ back-end/atividades/Numero_Secreto/Numero_Secreto_1.py
@@ -0,0 +1,9 @@
+
+numero_secreto = 7
+
+numero_usuario = input("digite um número de 0 a 25")
+
+if numero_usuario == 7 :
+    print("você acertou ")
+
+
--- back-end/atividades/Numero_Secreto_1.py
+++
@@ -1,9 +0,0 @@
-
-numero_secreto = 7
-
-numero_usuario = input("digite um número de 0 a 25")
-
-if numero_usuario == 7 :
-    print("você acertou ")
-
-
