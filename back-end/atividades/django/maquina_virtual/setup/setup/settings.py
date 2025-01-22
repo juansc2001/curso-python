3d7d35e20380ba8efee2343edabdc68d2ce03a7d
Juan_de_sousa_cardoso <juandesouzacardoso@outlook.com>
2025-01-22 14:00:08 -0300

    arquivo gitignore adcionado, algumas modificações

--- back-end/atividades/django/maquina_virtual/setup/setup/settings.py
+++ back-end/atividades/django/maquina_virtual/setup/setup/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lista.apps.listaConfig',
+    'lista.apps.ListaConfig',
     
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
